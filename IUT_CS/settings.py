b593772c571245ec58f4a4b9f34a70ecd0a3c6b7
Rafi Hassan Chowdhury <64986440+Grumpy-Frog@users.noreply.github.com>
2023-10-17 00:52:56 +0600

    Update settings.py turn off the site for temporarily

--- IUT_CS/settings.py
+++ IUT_CS/settings.py
@@ -98,7 +98,7 @@ LOGIN_REDIRECT_URL = 'admin_panel'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-IS_LOCALHOST = 2
+IS_LOCALHOST = 1
 
 if IS_LOCALHOST == 1:
     DATABASES = {
